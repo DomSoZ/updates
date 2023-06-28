d0ed5874a4f9e6d2d037384dd1c1300ef62651a7
Sunday-boop <dominikso1895@gmail.com>
2023-06-28 13:09:55 -0500

    second commit

--- app/Http/Controllers/ReferenciasController.php
+++ app/Http/Controllers/ReferenciasController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ReferenciasController extends Controller
 {
     public function index(){
-        return view('plantilla');
+        return view('welcome');
     }
 }
