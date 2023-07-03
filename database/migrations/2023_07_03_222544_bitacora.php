a2e58a7ea83be1690040acfbed1a017e341445a8
RobertoHerrOrt <97401440+RobertoHerrOrt@users.noreply.github.com>
2023-07-03 16:45:12 -0600

    03.07.2023
    migracion

---
+++ database/migrations/2023_07_03_222544_bitacora.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bitacora', function (Blueprint $table) {
+            $table->id();
+            $table->string('id_usuario');
+            $table->string('linea_captura');
+            $table->string('accion');
+            $table->dateTime('fecha');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bitacora');
+    }
+};
